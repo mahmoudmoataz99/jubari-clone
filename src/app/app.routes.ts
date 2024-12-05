29c57e70b3c0025c49c56753d94db2b721db5174
mahmoudmoataz99 <mahmoudmoataz99@gmail.com>
2024-12-05 19:31:05 +0200

    zbtt shwyt 7gat

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
-    {path:'about',component:AboutComponent}
+    {path:'about',component:AboutComponent},
+    {path:'**',component:ErrorComponent}
 ];
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports:[RouterLink],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router:Router){}
+  ngOnInit(): void {
+  }
+
 }
