f1557aaa0d426566846cfea0b24a842564c8e79d
mahmoudmoataz99 <mahmoudmoataz99@gmail.com>
2024-12-08 10:34:25 +0200

    10:34

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-item',
   standalone: true,
@@ -8,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './item.component.css'
 })
 export class ItemComponent {
-
+  @Input() title?: string;
+  @Input() imageUrl?: string;
+  @Input() link?: string;
 }
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -9,5 +9,10 @@ import { ItemComponent } from '../../components/item/item.component';
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
-
+  appItems = [
+    { title: 'حجز الفنادق', imageUrl: 'https://jubari.net/media/886/oswald-elsaboath-7EDZ1fxhgJQ-unsplash-(1).jpg', link: 'https://jubari.net/ar/services/2' },
+    { title: 'النقل البرى', imageUrl: 'https://jubari.net/media/864/jonathan-borba-T5jzpRTVF1U-unsplash.jpg', link: 'https://jubari.net/ar/services/3' },
+    { title: 'تأجير السيارات', imageUrl: 'https://jubari.net/media/872/tim-depickere-5mp2mlab7ZM-unsplash.jpg', link: 'https://jubari.net/ar/services/4' },
+    { title: 'تأمين السفر', imageUrl: 'https://jubari.net/media/870/jeshoots-com-mSESwdMZr-A-unsplash.jpg', link: 'https://jubari.net/ar/services/14' },
+  ];
 }
